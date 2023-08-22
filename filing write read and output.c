fb28730dc01ae8597b6c2544d42f9a3acec4d5e7
Daniyal Faisal <94481598+daniyal-faisal@users.noreply.github.com>
2023-08-23 03:06:16 +0500

    Create filing write read and output.c

---
+++ filing write read and output.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main()
+    {
+        FILE *file = fopen("data.txt", "a");
+
+        while (1)
+        {
+            char name[255], section;
+            int Class;
+            double GPA;
+
+            printf("Name:");
+            fgets(name, 255, stdin);
+            if (name[0] == '\n')
+            {
+                break;
+            }
+
+            printf("Class:");
+            scanf("%d", &Class);
+
+            printf("Section:");
+            scanf(" %c", &section);
+
+            printf("GPA:");
+            scanf("%lf", &GPA);
+
+            getchar();
+
+            fprintf(file, "Name:%s", name);
+            fprintf(file, "Class:%d\n", Class);
+            fprintf(file, "Section:%c\n", section);
+            fprintf(file, "GPA:%f\n", GPA);
+            fprintf(file, "\n");
+        }
+
+    fclose(file);
+
+    file = fopen("data.txt", "r");
+    char line[255];
+    printf("\n");
+    printf("Output:\n");
+    while(fgets(line, 255, file) != NULL)
+    {
+        printf("%s", line);
+    }
+
+    return 0;
+}
